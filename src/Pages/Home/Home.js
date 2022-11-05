e240b91d03f63e68509ef0124cb9589759334057
YousefMishael <yrysmq@gmail.com>
2022-11-05 12:29:17 +0200

    posts form

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -9,7 +9,9 @@ function Home() {
         setContent(e.target.value);
     }
 
-    const addPost = () => {
+    const addPost = (e) => {
+
+        e.preventDefault();
 
         if(content === '')
             return;
@@ -24,10 +26,12 @@ function Home() {
 
   return (
     <div className='d-flex flex-column align-items-center mt-5'>
-        <textarea className='w-50 form-control' value={content} onChange={onContentChange} rows={5} placeholder='type something...' />    
-        <div className='d-flex alig-items-start w-50'>
-            <button className='mt-2 btn btn-success' onClick={addPost}>Add Post</button>
-        </div>
+        <form className='w-50 form' onSubmit={addPost}>
+            <textarea className='w-100 form-control' value={content} onChange={onContentChange} rows={5} placeholder='type something...' />    
+            <div className='d-flex alig-items-start w-50'>
+                <button className='mt-2 btn btn-success' >Add Post</button>
+            </div>
+        </form>
     </div>
   )
 }
